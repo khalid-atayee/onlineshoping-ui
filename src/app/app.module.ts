36ce5c8150d3e79f205ad174ba58477794a3a56a
khalid-atayee <khalidatayee.76@gmail.com>
2022-10-12 23:56:15 +0430

    sign up validation completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/pages/signup/signup.component';
+import { SigninComponent } from './components/pages/signin/signin.component';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    SigninComponent,
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule
+    ,
+
+    RouterModule.forRoot([
+      {path:'',component:SigninComponent},
+      {path:'signUp',component:SignupComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/pages/signup/signup.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  userForm:any;
+  constructor(private fb: FormBuilder) { 
+  }
+  ngOnInit(): void {
+    this.userForm=this.userForm=this.fb.group({
+      username:['', [Validators.required,Validators.minLength(3)]],
+      email:['',[Validators.required,Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]],
+      password : ['',[Validators.required,Validators.minLength(8)]],
+      cpassword:['',[Validators.required,Validators.minLength(8)]],
+      term : ['',Validators.required]
+    })
+  }
+  get username(){
+    return this.userForm.get('username')
+  }
+  get email(){
+    return this.userForm.get('email')
+  }
+  get password(){
+    return this.userForm.get('password')
+  }
+  get cpassword(){
+    return this.userForm.get('cpassword')
+  }
+  get term(){
+    return this.userForm.get('term')
+  }
+
+  signupData(){
+    console.log(this.userForm.value);
+    // console.log('ok')
+
+  }
+
+
+}
+
+
